8190036b973d0a6b82e3c2cd5310057a566178a2
Rebai Mohamed <mohamedrebai748@gmail.com>
2025-05-05 20:43:10 +0100

    feat: setup for metrics collection and logging with Fluentd and Kafka

--- metrics_collector/collector.py
+++ metrics_collector/collector.py
@@ -1,7 +1,15 @@
+import os
 import psutil
 import time
 import socket
 import json
+from confluent_kafka import Producer
+
+KAFKA_BROKER = os.environ.get("KAFKA_BROKER", "localhost:9092") 
+KAFKA_TOPIC = os.environ.get("KAFKA_TOPIC","metrics")
+
+
+
 
 def get_system_metrics():
     # CPU usage
@@ -31,11 +39,30 @@ def get_system_metrics():
         "timestamp": time.time()
     }
 
-def send_metrics(metrics):
-    print(json.dumps(metrics, indent=2))
+def delivery_report(err, msg):
+    """Callback to check if message was successfully published."""
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Metrics published to {msg.topic()} [Partition {msg.partition()}]")
+
+
+def send_metrics(producer, metrics):
+    metrics_json = json.dumps(metrics)   
+    producer.produce(
+        topic=KAFKA_TOPIC,
+        value=metrics_json,
+        callback=delivery_report
+    )
+    producer.flush()  
 
 if __name__ == "__main__":
+    conf = {
+        "bootstrap.servers": KAFKA_BROKER,
+        "client.id": socket.gethostname()
+    }
+    producer = Producer(conf)
     while True:
         metrics = get_system_metrics()
-        send_metrics(metrics)
+        send_metrics(producer,metrics)
         time.sleep(2)  # Collect and send every 2 seconds
